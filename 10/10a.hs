596e92924dc35b64c0c56d0835c5fcb5e692b898
Filip Hallqvist <filiphallqvist@gmail.com>
2017-01-11 18:51:12 +0100

    Probably fucked it up even more... But we're getting there...

--- 10/10a.hs
+++ 10/10a.hs
@@ -1,19 +1,30 @@
-data Robot = Microbot Chip Chip Dest Dest
+import Data.List
+
+data Robot = Microbot Int Chip Chip
   deriving (Eq, Show)
 
 data Chip = NilChip | Chip Int
-  deriving (Eq, Show)
+  deriving (Eq, Show, Ord)
 
 data Dest = NilDest | Robot Int | Output Int
   deriving (Eq, Show)
 
 type Command = (Int, Dest, Dest)
 
-loadChip :: Robot -> Chip -> Robot
-loadChip (Microbot NilChip NilChip d1 d2) c = Microbot c NilChip d1 d2
-loadChip (Microbot c1 NilChip d1 d2) c2 = Microbot c1 c2 d1 d2
-loadChip r c = error "bot capacity overflow"
 
+-- Load a chip onto a bot
+loadChip :: Robot -> Chip -> Robot
+loadChip (Microbot id NilChip NilChip) c = Microbot id c NilChip
+loadChip (Microbot id c1 NilChip) c2 = Microbot id c1 c2
+loadChip r c = error "loadChip: bot capacity overflow"
+
+-- Search a list for a bot
+findBotById :: Int -> [(Int, Robot)] -> (Int, Robot)
+findBotById id bots = case lookup id bots of
+  Just b -> (id, b)
+  Nothing -> error "findBotById: bot not found"
+
+-- Initialize robot with starting values
 initRobots :: [(Int, Robot)] -> String -> [(Int, Robot)]
 initRobots bots s = (id, loadChip bot (Chip val)) : bots
   where
@@ -21,10 +32,11 @@ initRobots bots s = (id, loadChip bot (Chip val)) : bots
     val = read $ w !! 1
     id = read $ w !! 5
     bot = case lookup id bots of
-      Just (Microbot (Chip c) NilChip NilDest NilDest) -> Microbot (Chip c) NilChip NilDest NilDest
-      Nothing -> Microbot NilChip NilChip NilDest NilDest
-      otherwise -> error "bot capacity overflow"
+      Just (Microbot id (Chip c) NilChip) -> Microbot id (Chip c) NilChip
+      Nothing -> Microbot id NilChip NilChip
+      otherwise -> error "initRobots: bot capacity overflow"
 
+-- Parse a command into a triple (Bot id, Low destination, High destination)
 parseCommand :: String -> Command
 parseCommand s = (id, d1, d2)
   where
@@ -34,37 +46,41 @@ parseCommand s = (id, d1, d2)
     d1 = parseDest (w !! 5) . read $ w !! 6
     d2 = parseDest (w !! 10) . read $ w !! 11
 
-runCommand :: [(Int, Robot)] -> Command -> (Int, Robot)
-runCommand bots (id, d1, d2) = (id, bot)
-  where
-    bot = case lookup id bots of
-      Just (Microbot c1 c2 NilDest NilDest) -> Microbot c1 c2 d1 d2
-      Nothing -> Microbot NilChip NilChip d1 d2
-      otherwise -> error "bot already commanded"
+-- Checks if a bot is fully loaded
+isFullyLoaded :: Robot -> Bool
+isFullyLoaded (Microbot _ (Chip c1) (Chip c2)) = True
+isFullyLoaded (Microbot _ _ _) = False
 
-isLoaded :: (Int, Robot) -> Bool
-isLoaded (_, (Microbot NilChip NilChip _ _)) = False
-isLoaded bot = True
-
-launchRobot :: [(Int, Robot)] -> (Int, Robot) -> [(Int, Robot)]
-launchRobot bots (id, (Microbot c1 c2 d1 d2)) = 
+-- Run a command
+runCommand :: [(Int, Robot)] -> Command -> [(Int, Robot)]
+runCommand robots (id, d1, d2) = if isFullyLoaded bot then deliverChip bot (id, d1, d2) robots else robots
   where
-    bot1 = case lookup id bots of
-      Just (Microbot c1 c2 d1 d2) = Microbot c1 c2 d1 d2
-      Nothing = error "bot not found"
-      
+    (_, bot) = case lookup id robots of
+      Just b -> findBotById id robots
+      Nothing -> (id, Microbot id NilChip NilChip) -- If bot is non-existent, just move on
+
+-- Deliver chips from a bot to other bots
+deliverChip :: Robot -> Command -> [(Int, Robot)] -> [(Int, Robot)]
+deliverChip (Microbot _ NilChip NilChip) _ _ = error "deliverChip: bot not fully loaded"
+deliverChip (Microbot _ c1 c2) (id, d1, d2) robots = addedBots
+  where
+    low = min c1 c2
+    high = max c1 c2
+    (lId, Microbot _ l1 _) = findBotById d1 robots
+    (hId, Microbot _ h1 _) = findBotById d2 robots
+    deletedBots = delete (id, Microbot id c1 c2) . delete (lId, Microbot lId l1 NilChip) . delete (hId, Microbot hId h1 NilChip) $ robots
+    addedBots = (id, Microbot id NilChip NilChip):(d1, Microbot d1 l1 low):(d2, Microbot d2 h1 high):deletedBots
 
 main = do
   input <- lines <$> readFile "input"
   
   let initValues = filter ((=='v') . head) input
+  let initialized = foldl initRobots [] $ initValues
+
   let commands = map parseCommand $ filter ((=='b') . head) input
+  let launched = foldl runCommand initialized commands
 
-  let initialized = foldl initRobots [] initValues
-  let aimed = filter isLoaded . map (runCommand initialized) $ commands
-
-  let launched = foldl launchRobot aimed aimed
-
-  mapM (putStrLn . show) aimed
+  mapM (putStrLn . show) initialized 
+  mapM (putStrLn . show) commands
 
   putStrLn "done"
